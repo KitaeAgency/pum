fdff27989e96e4c7c21e9a14ed874babdd549d05
alexandresalome <alexandre.salome@gmail.com>
2013-07-19 11:43:52 +0200

    wip

--- src/Pum/Core/Extension/EmFactory/Doctrine/ObjectEntityManager.php
+++ src/Pum/Core/Extension/EmFactory/Doctrine/ObjectEntityManager.php
@@ -12,28 +12,21 @@ use Pum\Core\SchemaManager;
 
 class ObjectEntityManager extends EntityManager
 {
-    protected function __construct(SchemaManager $schemaManager, Connection $conn, Configuration $config, EventManager $eventManager)
-    {
-        $config->setClassMetadataFactoryName('Pum\Core\Extension\EmFactory\Doctrine\Metadata\ObjectClassMetadataFactory');
-
-        parent::__construct($conn, $config, $eventManager);
-
-        $this->getMetadataFactory()->setSchemaManager($schemaManager);
-    }
-
     public function createObject($name)
     {
         $class = $this->getRepository($name)->getClassname();
-
-        var_dump($class);exit;
     }
 
     public static function createPum(SchemaManager $schemaManager, Connection $conn, $projectName)
     {
         $config = Setup::createConfiguration();
         $config->setMetadataDriverImpl(new PumDefinitionDriver($schemaManager, $projectName));
+        $config->setClassMetadataFactoryName('Pum\Core\Extension\EmFactory\Doctrine\Metadata\ObjectClassMetadataFactory');
         // later, cache metadata here
 
-        return new ObjectEntityManager($schemaManager, $conn, $config, $conn->getEventManager());
+        $em = new ObjectEntityManager($conn, $config, $conn->getEventManager());
+        $em->getMetadataFactory()->setSchemaManager($schemaManager);
+
+        return $em;
     }
 }
