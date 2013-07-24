cc60cc59d1229888c734a16f7b9c75cc307e3753
Phuc Tran <tpt@les-argonautes.fr>
2013-07-24 14:57:40 +0200

    crud beam in progress

--- src/Pum/Bundle/WoodworkBundle/Controller/BeamController.php
+++ src/Pum/Bundle/WoodworkBundle/Controller/BeamController.php
@@ -6,6 +6,7 @@ use Pum\Core\Exception\BeamNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Pum\Bundle\WoodworkBundle\Form\Type\ObjectDefinitionType;
 use Symfony\Component\HttpFoundation\Request;
+use Pum\Core\Definition\Beam;
 
 class BeamController extends Controller
 {
@@ -21,11 +22,10 @@ class BeamController extends Controller
         $manager = $this->get('pum');
 
         $form = $this->createForm('ww_beam_definition');
-        if ($request->getMethod() == 'POST') {
-            if ($form->bind($request)->isValid()) {
+        if ($request->isMethod('POST') && $form->bind($request)->isValid()) {
+    		$manager->saveBeam($form->getData());
 
-                return $this->redirect($this->generateUrl('ww_beam_definition_list'));
-            }
+            return $this->redirect($this->generateUrl('ww_beam_definition_list'));
         }
 
         return $this->render('PumWoodworkBundle:Beam:create.html.twig', array(
