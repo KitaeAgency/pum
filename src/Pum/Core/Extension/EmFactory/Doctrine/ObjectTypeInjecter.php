221413642a6a885364c89720ba76527ef69da2ac
alexandresalome <alexandre.salome@gmail.com>
2013-07-26 17:48:41 +0200

    add missing class

---
+++ src/Pum/Core/Extension/EmFactory/Doctrine/ObjectTypeInjecter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pum\Core\Extension\EmFactory\Doctrine;
+
+use Doctrine\Common\EventArgs;
+use Doctrine\Common\EventSubscriber;
+use Pum\Core\Type\Factory\TypeFactoryInterface;
+
+class ObjectTypeInjecter implements EventSubscriber
+{
+    protected $typeFactory;
+
+    public function __construct(TypeFactoryInterface $typeFactory)
+    {
+        $this->typeFactory = $typeFactory;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array('postLoad');
+    }
+
+    public function postLoad(EventArgs $event)
+    {
+        $event->getEntity()->__pum__setTypes($this->typeFactory);
+    }
+}
