504d3a69fb88b83ec8f41467276a89561eaf5273
alphonsediouf <ad@les-argonautes.fr>
2013-08-12 10:20:54 +0200

    Revert "cloning a beam"
    This reverts commit 328c4786e0c393db82e37b4bfc8456a852c4e87e.

--- src/Pum/Bundle/WoodworkBundle/Controller/BeamController.php
+++ src/Pum/Bundle/WoodworkBundle/Controller/BeamController.php
@@ -6,7 +6,6 @@ use Pum\Core\Definition\Beam;
 use Pum\Core\Exception\BeamNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -17,6 +16,8 @@ class BeamController extends Controller
      */
     public function listAction()
     {
+        $this->assertGranted('ROLE_WW_BEAMS');
+
         return $this->render('PumWoodworkBundle:Beam:list.html.twig', array(
             'beams' => $this->get('pum')->getAllBeams()
         ));
@@ -27,6 +28,8 @@ class BeamController extends Controller
      */
     public function createAction(Request $request)
     {
+        $this->assertGranted('ROLE_WW_BEAMS');
+
         $manager = $this->get('pum');
 
         $form = $this->createForm('ww_beam');
@@ -47,6 +50,8 @@ class BeamController extends Controller
      */
     public function editAction(Request $request, Beam $beam)
     {
+        $this->assertGranted('ROLE_WW_BEAMS');
+
         $manager  = $this->get('pum');
         $beamView = clone $beam;
 
@@ -64,43 +69,13 @@ class BeamController extends Controller
     }
 
     /**
-     * @Route(path="/beams/{beamName}/clone", name="ww_beam_clone")
-     * @ParamConverter("beam", class="Beam")
-     */
-    public function cloneAction(Request $request, Beam $beam)
-    {
-        $manager  = $this->get('pum');
-        $beamView = clone $beam;
-
-        $form = $this->createForm('ww_beam');
-        if ($request->isMethod('POST') && $form->bind($request)->isValid()) {
-
-            $clone = Beam::create($form->getData()->getName());
-
-            foreach ($beam->getObjects() as $object) {
-                $clone->addObject(clone $object);
-            }
-            foreach ($beam->getRelations() as $relation) {
-                $clone->addRelation(clone $relation);
-            }
-
-            $manager->saveBeam($clone);
-
-            return $this->redirect($this->generateUrl('ww_beam_list'));
-        }
-
-        return $this->render('PumWoodworkBundle:Beam:clone.html.twig', array(
-            'beam' => $beamView,
-            'form' => $form->createView()
-        ));
-    }
-
-    /**
      * @Route(path="/beams/{beamName}/delete", name="ww_beam_delete")
      * @ParamConverter("beam", class="Beam")
      */
     public function deleteAction(Beam $beam)
     {
+        $this->assertGranted('ROLE_WW_BEAMS');
+
         $manager = $this->get('pum');
 
         if (!$beam->isDeletable()) {
