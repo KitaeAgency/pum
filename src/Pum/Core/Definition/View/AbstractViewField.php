9cd8be1a3f1232f22b1816a639fd1abd6c0851e0
Phuky <tpt@les-argonautes.com>
2013-09-20 18:58:24 +0200

    Refactor abstractViewField

--- src/Pum/Core/Definition/View/AbstractViewField.php
+++ src/Pum/Core/Definition/View/AbstractViewField.php
@@ -2,6 +2,8 @@
 
 namespace Pum\Core\Definition\View;
 
+use Pum\Core\Definition\FieldDefinition;
+
 abstract class AbstractViewField
 {
     /**
@@ -18,4 +20,88 @@ abstract class AbstractViewField
      * @var FieldDefinition
      */
     protected $fieldDefinition;
+
+    /**
+     * @var string
+     */
+    protected $view;
+
+    /**
+     * @var integer
+     */
+    protected $sequence;
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @return TableViewField
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return FieldDefinition
+     */
+    public function getFieldDefinition()
+    {
+        return $this->fieldDefinition;
+    }
+
+    /**
+     * Changes associated fieldDefinition.
+     *
+     * @return TableViewField
+     */
+    public function setFieldDefinition(FieldDefinition $fieldDefinition = null)
+    {
+        $this->fieldDefinition = $fieldDefinition;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * @return TableViewField
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getSequence()
+    {
+        return $this->sequence;
+    }
+
+    /**
+     * @return TableViewField
+     */
+    public function setSequence($sequence)
+    {
+        $this->sequence = $sequence;
+
+        return $this;
+    }
 }
--- src/Pum/Core/Definition/View/TableViewField.php
+++ src/Pum/Core/Definition/View/TableViewField.php
@@ -14,16 +14,6 @@ class TableViewField extends AbstractViewField
     protected $tableview;
 
     /**
-     * @var string
-     */
-    protected $view;
-
-    /**
-     * @var integer
-     */
-    protected $sequence;
-
-    /**
      * Constructor.
      */
     public function __construct($label = null, FieldDefinition $fieldDefinition = null, $view = self::DEFAULT_VIEW, $sequence = null)
@@ -35,7 +25,7 @@ class TableViewField extends AbstractViewField
     }
 
     /**
-     * @return ObjectDefinition
+     * @return TableViewField
      */
     public static function create($label = null, FieldDefinition $fieldDefinition = null, $view = self::DEFAULT_VIEW, $sequence = null)
     {
@@ -58,80 +48,6 @@ class TableViewField extends AbstractViewField
     public function setTableview(Tableview $tableview = null)
     {
         $this->tableview = $tableview;
-
-        return $this;
-    }
-
-    /**
-     * @return FieldDefinition
-     */
-    public function getFieldDefinition()
-    {
-        return $this->fieldDefinition;
-    }
-
-    /**
-     * Changes associated fieldDefinition.
-     *
-     * @return TableViewField
-     */
-    public function setFieldDefinition(FieldDefinition $fieldDefinition = null)
-    {
-        $this->fieldDefinition = $fieldDefinition;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLabel()
-    {
-        return $this->label;
-    }
-
-    /**
-     * @return TableViewField
-     */
-    public function setLabel($label)
-    {
-        $this->label = $label;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getView()
-    {
-        return $this->view;
-    }
-
-    /**
-     * @return TableViewField
-     */
-    public function setView($view)
-    {
-        $this->view = $view;
-
-        return $this;
-    }
-
-    /**
-     * @return integer
-     */
-    public function getSequence()
-    {
-        return $this->sequence;
-    }
-
-    /**
-     * @return TableViewField
-     */
-    public function setSequence($sequence)
-    {
-        $this->sequence = $sequence;
 
         return $this;
     }
