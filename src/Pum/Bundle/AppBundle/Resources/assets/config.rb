189fd768a4395e5b04bee9ccdbac2759c0b1c56b
Christophe VERGNE <christophe.vergne@gmail.com>
2013-10-08 17:18:56 +0200

    Cleaning & Organizing Sass files

--- src/Pum/Bundle/AppBundle/Resources/assets/config.rb
+++ src/Pum/Bundle/AppBundle/Resources/assets/config.rb
@@ -22,7 +22,6 @@ javascripts_dir = "../public/js"
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
-sass_options = {:debug_info=>false} # by Compass.app
-
 
 output_style = :compressed # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
