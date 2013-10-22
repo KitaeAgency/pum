c11f265174913eae702e5f123c8074dba5a4c961
Christophe VERGNE <christophe.vergne@gmail.com>
2013-10-22 10:36:17 +0200

    UI - Update icons + Adjust sidebar size
    * Update icon font and icon font config from new icomoon app version

--- src/Pum/Bundle/AppBundle/Resources/public/js/pum.collection.js
+++ src/Pum/Bundle/AppBundle/Resources/public/js/pum.collection.js
@@ -8,7 +8,7 @@ pum_decorateCollection = (function() {
     var outAnimatedClass = 'fadeOutRight ' + commonAnimatedClass;
 
     // Action buttons
-    var createLink  = '<hr/><a href="#" class="btn pum-scheme-btn-darkgrass btn-mini btn-action pull-right"><i class="pumicon pumicon-plus-3"></i> Add</a>';
+    var createLink  = '<hr/><a href="#" class="btn pum-scheme-btn-darkgrass btn-mini btn-action pull-right"><i class="pumicon pumicon-plus3"></i> Add</a>';
     var deleteLink  = '<a href="#" class="pull-right panel-control pum-scheme-link-pomegranate"><i class="pumicon pumicon-trashcan"></i></a>';
 
     // Wrapping backup
