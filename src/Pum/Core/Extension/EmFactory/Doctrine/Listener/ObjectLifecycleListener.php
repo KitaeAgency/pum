d464ac4a74189f2c21d3e5be7389663db493ce0e
alexandresalome <alexandre.salome@gmail.com>
2013-10-31 10:47:44 +0100

    delay event dispatching of newly created objects

--- src/Pum/Core/Extension/EmFactory/Doctrine/Listener/ObjectLifecycleListener.php
+++ src/Pum/Core/Extension/EmFactory/Doctrine/Listener/ObjectLifecycleListener.php
@@ -4,6 +4,7 @@ namespace Pum\Core\Extension\EmFactory\Doctrine\Listener;
 
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Pum\Core\Event\ObjectEvent;
 use Pum\Core\Events;
 use Pum\Core\ObjectFactory;
@@ -12,6 +13,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 class ObjectLifecycleListener implements EventSubscriber
 {
     protected $factory;
+    protected $pendingInserts = array();
 
     public function __construct(ObjectFactory $factory)
     {
@@ -23,7 +25,7 @@ class ObjectLifecycleListener implements EventSubscriber
      */
     public function getSubscribedEvents()
     {
-        return array('onFlush');
+        return array('onFlush', 'postFlush');
     }
 
     public function onFlush(OnFlushEventArgs $args)
@@ -31,9 +33,7 @@ class ObjectLifecycleListener implements EventSubscriber
         $em = $args->getEntityManager();
         $uow = $em->getUnitOfWork();
 
-        foreach ($uow->getScheduledEntityInsertions() as $insert) {
-            $this->factory->getEventDispatcher()->dispatch(Events::OBJECT_CHANGE, new ObjectEvent($insert, $this->factory));
-        }
+        $this->pendingInserts = $uow->getScheduledEntityInsertions();
 
         foreach ($uow->getScheduledEntityUpdates() as $update) {
             $this->factory->getEventDispatcher()->dispatch(Events::OBJECT_CHANGE, new ObjectEvent($update, $this->factory));
@@ -43,5 +43,17 @@ class ObjectLifecycleListener implements EventSubscriber
             $this->factory->getEventDispatcher()->dispatch(Events::OBJECT_DELETE, new ObjectEvent($delete, $this->factory));
         }
 
+    }
+
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($this->pendingInserts as $insert) {
+            $this->factory->getEventDispatcher()->dispatch(Events::OBJECT_CHANGE, new ObjectEvent($insert, $this->factory));
+        }
+
+        $this->pendingInserts = array();
     }
 }
