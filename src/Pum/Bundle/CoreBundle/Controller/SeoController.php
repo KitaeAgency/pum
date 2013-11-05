4101d54fe06a6b25033fe07e1f848e9c3ca8c443
Phuky <tpt@les-argonautes.com>
2013-11-05 15:56:29 +0100

    delia seo like

--- src/Pum/Bundle/CoreBundle/Controller/SeoController.php
+++ src/Pum/Bundle/CoreBundle/Controller/SeoController.php
@@ -5,41 +5,96 @@ namespace Pum\Bundle\CoreBundle\Controller;
 use Pum\Core\Extension\Routing\RoutableInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SeoController extends Controller
 {
     /**
      * @Route(name="pum_object", path="/{_project}/{key}", requirements={"key"=".*"})
      */
-    public function showAction($key)
+    public function showAction($key, Request $request)
     {
+        /* Delia Seo Style */
+
+        // Vars for template
+        $vars = array();
+
+        // Get all parts from url
         $parts = explode('/', $key);
-        $seoKey = array_pop($parts);
-        $id = $this->get('pum.routing')->getRoutingTable()->match($seoKey);
 
-        if (null === $id) {
-            throw $this->createNotFoundException('No element in routing table with key '.$seoKey);
+        // Convert all parts into objects or vars
+        foreach ($parts as $part) {
+            if (false !== strpos($part, '=')) {
+                $this->addVarToVars($vars, $part);
+            } else {
+                $this->addObjectToVars($vars, $part);
+            }
         }
 
-        if (false === strpos($id, ':')) {
-            throw new \RuntimeException('Unexpected value found in routing table: "'.$id.'".');
+        // Get vars from request
+        $vars  = array_merge($vars, $request->query->all());
+
+        // Get template
+        $templateName = $this->getTemplateFromVars($vars);
+
+        return $this->render($templateName, $vars);
+    }
+
+    private function addObjectToVars(&$vars, $seoKey)
+    {
+        if ($seoKey) {
+            $id = $this->get('pum.routing')->getRoutingTable()->match($seoKey);
+
+            if (null === $id) {var_dump($seoKey);die;
+                throw $this->createNotFoundException('No element in routing table with key '.$seoKey);
+            }
+
+            if (false === strpos($id, ':')) {
+                throw new \RuntimeException('Unexpected value found in routing table: "'.$id.'".');
+            }
+
+            list($objClass, $objId) = explode(':', $id, 2);
+
+            $object = $this->get('pum.oem')->getRepository($objClass)->find($objId);
+
+            if (!$object) {
+                throw new \RuntimeException('Incorrect value found in routing table: "'.$id.'". Maybe object does not exist?');
+            }
+
+            if (!$object instanceof RoutableInterface) {
+                throw new \RuntimeException('Expected a RoutableInterface object, got a '.get_class($object));
+            }
+
+            foreach (array($objClass, 'object') as $key) {
+                if (isset($vars[$key])) {
+                    $i = 2;
+                    while (isset($vars[$key.$i])) {
+                        $i++;
+                    }
+                    $key = $key.$i;
+                }
+
+                $vars[$key] = $object;
+            }
+        }
+    }
+
+    private function addVarToVars(&$vars, $var)
+    {
+        $data   = explode('=', $var, 2);
+        $vars[$data[0]] = $data[1];
+    }
+
+    private function getTemplateFromVars($vars)
+    {
+        foreach ($vars as $object) {
+            if (is_object($object)) {
+                if ($template = $object->getSeoTemplate()) {
+                    return $template;
+                }
+            }
         }
 
-        list($objClass, $objId) = explode(':', $id, 2);
-
-        $object = $this->get('pum.oem')->getRepository($objClass)->find($objId);
-
-        if (!$object) {
-            throw new \RuntimeException('Incorrect value found in routing table: "'.$id.'". Maybe object does not exist?');
-        }
-
-        if (!$object instanceof RoutableInterface) {
-            throw new \RuntimeException('Expected a RoutableInterface object, got a '.get_class($object));
-        }
-
-        return $this->render($object->getSeoTemplate(), array(
-            $objClass => $object,
-            'object'  => $object
-        ));
+        throw $this->createNotFoundException('No template found in objects !');
     }
 }
