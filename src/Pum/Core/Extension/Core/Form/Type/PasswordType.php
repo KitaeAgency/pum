9e285cdbf6d585f1bd7173d80091205e7a492eec
alexandresalome <alexandre.salome@gmail.com>
2014-01-14 11:07:03 +0100

    add feature repeated on password field

--- src/Pum/Core/Extension/Core/Form/Type/PasswordType.php
+++ src/Pum/Core/Extension/Core/Form/Type/PasswordType.php
@@ -2,11 +2,15 @@
 
 namespace Pum\Core\Extension\Core\Form\Type;
 
+use Pum\Core\Definition\View\FormView;
 use Pum\Core\Extension\Util\Namer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView as FormFormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Symfony\Component\Security\Core\Encoder\EncoderFactory;
@@ -24,19 +28,31 @@ class PasswordType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        if ($options['repeated']) {
+            $this->buildFormRepeated($builder, $options);
+        } else {
+            $this->buildFormSimple($builder, $options);
+        }
+    }
+
+    public function buildView(FormFormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['repeated'] = $options['repeated'];
+    }
+
+    public function buildFormSimple(FormBuilderInterface $builder, array $options)
+    {
         $ef = $this->encoderFactory;
 
         $builder
-            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($ef) {
+            ->add('single', 'password')
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($ef) {
                 $form = $event->getForm();
 
-                if (!$form->isValid()) {
-                    return;
-                }
+                $passwords = $event->getData();
 
-                $password = $form->getData();
-
-                if (!$password) {
+                // empty password
+                if (!$passwords['single']) {
                     return;
                 }
 
@@ -48,7 +64,45 @@ class PasswordType extends AbstractType
                     return;
                 }
 
-                $root->$method($password, $ef);
+                $root->$method($passwords['single'], $ef);
+
+            })
+        ;
+    }
+
+    public function buildFormRepeated(FormBuilderInterface $builder, array $options)
+    {
+        $ef = $this->encoderFactory;
+
+        $builder
+            ->add('first', 'password')
+            ->add('second', 'password')
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($ef) {
+                $form = $event->getForm();
+
+                $passwords = $event->getData();
+
+                // not matching
+                if ($passwords['first'] !== $passwords['second']) {
+                    $form->addError(new FormError('Passwords don\'t match'));
+
+                    return;
+                }
+
+                // empty password
+                if (!$passwords['first']) {
+                    return;
+                }
+
+                $root = $form->getParent()->getData();
+                $name = $form->getName();
+                $method = 'set'.ucfirst(Namer::toCamelCase($name));
+
+                if (!method_exists($root, $method)) {
+                    return;
+                }
+
+                $root->$method($passwords['first'], $ef);
 
             })
         ;
@@ -60,16 +114,9 @@ class PasswordType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'mapped' => false
+            'mapped' => false,
+            'repeated' => false
         ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getParent()
-    {
-        return 'password';
     }
 
     /**
