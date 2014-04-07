da9fbc04d36b89376fce5f0aa9ca689befdec4f7
Christophe VERGNE <christophe.vergne@gmail.com>
2014-04-07 17:27:26 +0200

    Fix for Datetime field format

--- src/Pum/Core/Extension/Core/Type/DatetimeType.php
+++ src/Pum/Core/Extension/Core/Type/DatetimeType.php
@@ -3,10 +3,19 @@
 namespace Pum\Core\Extension\Core\Type;
 
 use Pum\Core\AbstractType;
+use Pum\Core\Context\FieldContext;
+use Pum\Core\Definition\View\FormViewField;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormInterface;
 
 class DatetimeType extends AbstractType
 {
+    const DATE_FORMAT    = "dd/MM/yyyy HH:mm:ss";
+    const JS_DATE_FORMAT = "dd/mm/yy HH:ii:ss";
+
+    const ANTERIOR_DATE  = 'anterior';
+    const POSTERIOR_DATE = 'posterior';
+
     /**
      * {@inheritdoc}
      */
@@ -14,6 +23,43 @@ class DatetimeType extends AbstractType
     {
         $resolver->setDefaults(array(
             '_doctrine_type' => 'datetime',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FieldContext $context, FormInterface $form, FormViewField $formViewField)
+    {
+        $restriction = $context->getOption('restriction');
+
+        if ($restriction === self::ANTERIOR_DATE) {
+            $yearsRange = "-70:+0";
+            $minDate = new \DateTime("-70 years");
+            $maxDate = new \DateTime();
+        } elseif ($restriction === self::POSTERIOR_DATE) {
+            $yearsRange = "-0:+70";
+            $minDate = new \DateTime();
+            $maxDate = new \DateTime("+70 years");
+        } else {
+            $yearsRange = "-35:+35";
+            $minDate = new \DateTime("-35 years");
+            $maxDate = new \DateTime("+35 years");
+        }
+
+        $form->add($context->getField()->getCamelCaseName(), 'date', array(
+            'widget' => 'single_text',
+            'format' => self::DATE_FORMAT,
+            'attr'   => array(
+                'class' => 'datepicker',
+                'data-yearrange'  => $yearsRange,
+                'data-mindate'    => $minDate->format("U"),
+                'data-maxdate'    => $maxDate->format("U"),
+                'data-dateFormat' => self::JS_DATE_FORMAT,
+                'placeholder'     => $formViewField->getPlaceholder()
+            ),
+            'label'    => $formViewField->getLabel(),
+            'required' => $context->getOption('required')
         ));
     }
 
