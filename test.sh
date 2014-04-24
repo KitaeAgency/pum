c53a0d906c9d328ebf53529848b9c3eb0aab0cbb
alexandresalome <alexandre.salome@gmail.com>
2014-04-24 13:19:41 +0200

    add test tools

--- test.sh
+++ test.sh
@@ -2,8 +2,11 @@
 set -e
 cd "`dirname "$0"`"
 
-./reset.sh
-bin/phpunit -c app
-echo "Running Behat, failed tests will appear below"
-mkdir -p app/cache/test-report
-bin/behat -f failed,journal --out null,app/cache/test-report.html
+
+if [ ! -f composer.phar ]; then
+    curl -s http://getcomposer.org/installer | php
+fi
+
+php composer.phar install
+
+bin/phpunit
