0bdd4b873d70bce5130d7e7cf537916fcc74cf84
Phuky <tpt@les-argonautes.com>
2014-09-18 14:13:49 +0200

    fix getFile for media

--- src/Pum/Bundle/TypeExtraBundle/Media/FilesystemStorage.php
+++ src/Pum/Bundle/TypeExtraBundle/Media/FilesystemStorage.php
@@ -68,7 +68,7 @@ class FilesystemStorage implements StorageInterface
 
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename='.Namer::toLowercase($filename.'.'.$extension));
+            header('Content-Disposition: attachment; filename='.Namer::toLowercase($filename).'.'.$extension);
             header('Content-Transfer-Encoding: binary');
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
