d542657316150410467c09a6eee151cff80c9972
Christophe VERGNE <christophe.vergne@gmail.com>
2014-12-08 15:16:21 +0100

    Handle Gmaps widget on Ajax requests

--- src/Pum/Bundle/AppBundle/Resources/public/js/pum.js
+++ src/Pum/Bundle/AppBundle/Resources/public/js/pum.js
@@ -193,7 +193,11 @@
         /* Yaah success event */
         $(document).on('yaah-js_xhr_beforeInsert', '.yaah-js', function(ev, eventId, target, item, data){
             $(document).one(eventId, function(ev, target, item, data){
-                $(target).find('.js-tatam').tatam();
+                var $target = $(target);
+
+                $target.find('.js-tatam').tatam();
+                window.input_gmaps_widget = $target.find('input[data-gmaps_widget]');
+                pum_load_mapsApi(input_gmaps_widget);
             });
         });
 
@@ -249,6 +253,82 @@
         pumDecorateHtml(document);
     });
 
+    window.pum_load_mapsApi = function(inputCollection)
+    {
+        if (inputCollection.length > 0) {
+            if (typeof google == 'undefined') {
+                // Inject Google Maps API w/ callback
+                var jq = document.createElement('script');
+                    jq.type = "text/javascript";
+                    jq.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places&callback=gmaps_loaded';
+                    $(document.body).append(jq);
+            } else {
+                pum_form_widget_gmaps(inputCollection);
+            }
+        }
+    }
+
+    window.pum_form_widget_gmaps = function(inputCollection)
+    {
+        $.each(inputCollection, function(index, input){
+            var par = $(input).parents('.form-group');
+            $(par.find('.panel-collapse')).one('shown.bs.collapse', function(ev){
+                par.addClass('gmaps_initialized');
+
+                // Retrieve lat/lng fields
+                var lat_field = par.find('input[data-gmaps_target=latitude]');
+                var lng_field = par.find('input[data-gmaps_target=longitude]');
+
+                var map_container = par.find('.pum_gmaps')[0];
+
+                // Initialize Google Maps
+                var map = new google.maps.Map(map_container, {
+                    center: new google.maps.LatLng(lat_field.val(), lng_field.val()),
+                    zoom: 17,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                par.data('gmaps_map', map);
+
+                // Initialize Autocomplete module
+                var autocomplete = new google.maps.places.Autocomplete(input);
+                autocomplete.bindTo('bounds', map);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    draggable:true,
+                    position: new google.maps.LatLng(lat_field.val(), lng_field.val())
+                });
+                par.data('gmaps_autocomplete', autocomplete);
+
+                // Initialize Autocomplete place change event
+                google.maps.event.addListener(autocomplete, 'place_changed',function(){
+                    var place = autocomplete.getPlace();
+                        if (place.geometry.viewport) {
+                            map.fitBounds(place.geometry.viewport);
+                        } else {
+                            map.setCenter(place.geometry.location);
+                            map.setZoom(17);
+                        }
+                        marker.setPosition(place.geometry.location);
+                        lat_field.val(place.geometry.location.lat());
+                        lng_field.val(place.geometry.location.lng());
+                });
+
+                // Avoid submitting form when pressing "enter" on autocomplete field
+                google.maps.event.addDomListener(input, 'keydown', function(e) {
+                    if (e.keyCode == 13) {
+                        e.preventDefault();
+                    }
+                });
+
+                // Initialize drag feature on marker to update coordinates
+                google.maps.event.addListener(marker, 'dragend', function() {
+                    var pos = marker.getPosition();
+                    lat_field.val(pos.lat());
+                    lng_field.val(pos.lng());
+                });
+            });
+        });
+    }
 
     /* DOMREADY
     -------------------------------------------------- */
@@ -367,74 +447,11 @@
 
         /* GMAPS Widget */
         window.input_gmaps_widget = $('input[data-gmaps_widget]');
+        window.gmaps_loaded = function() {
+            pum_form_widget_gmaps(input_gmaps_widget)
+        };
         if (input_gmaps_widget.length > 0) {
-            window.gmaps_loaded = function(){
-                $.each(input_gmaps_widget, function(index, input){
-                    var par = $(input).parents('.form-group');
-                    $(par.find('.panel-collapse')).one('shown.bs.collapse', function(ev){
-                        par.addClass('gmaps_initialized');
-
-                        // Retrieve lat/lng fields
-                        var lat_field = par.find('input[data-gmaps_target=latitude]');
-                        var lng_field = par.find('input[data-gmaps_target=longitude]');
-
-                        var map_container = par.find('.pum_gmaps')[0];
-
-                        // Initialize Google Maps
-                        var map = new google.maps.Map(map_container, {
-                            center: new google.maps.LatLng(lat_field.val(), lng_field.val()),
-                            zoom: 17,
-                            mapTypeId: google.maps.MapTypeId.ROADMAP
-                        });
-                        par.data('gmaps_map', map);
-
-                        // Initialize Autocomplete module
-                        var autocomplete = new google.maps.places.Autocomplete(input);
-                        autocomplete.bindTo('bounds', map);
-                        var marker = new google.maps.Marker({
-                            map: map,
-                            draggable:true,
-                            position: new google.maps.LatLng(lat_field.val(), lng_field.val())
-                        });
-                        par.data('gmaps_autocomplete', autocomplete);
-
-                        // Initialize Autocomplete place change event
-                        google.maps.event.addListener(autocomplete, 'place_changed',function(){
-                            var place = autocomplete.getPlace();
-                                if (place.geometry.viewport) {
-                                    map.fitBounds(place.geometry.viewport);
-                                } else {
-                                    map.setCenter(place.geometry.location);
-                                    map.setZoom(17);
-                                }
-                                marker.setPosition(place.geometry.location);
-                                lat_field.val(place.geometry.location.lat());
-                                lng_field.val(place.geometry.location.lng());
-                        });
-
-                        // Avoid submitting form when pressing "enter" on autocomplete field
-                        google.maps.event.addDomListener(input, 'keydown', function(e) {
-                            if (e.keyCode == 13) {
-                                e.preventDefault();
-                            }
-                        });
-
-                        // Initialize drag feature on marker to update coordinates
-                        google.maps.event.addListener(marker, 'dragend', function() {
-                            var pos = marker.getPosition();
-                            lat_field.val(pos.lat());
-                            lng_field.val(pos.lng());
-                        });
-                    });
-
-                });
-            };
-
-            // Inject Google Maps API w/ callback
-            var jq = document.createElement('script');
-                jq.type = "text/javascript";
-                jq.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places&callback=gmaps_loaded';
-                $(document.body).append(jq);
+            pum_load_mapsApi(input_gmaps_widget);
         } // end: GMAPS
 
         /* TATAM JS */
