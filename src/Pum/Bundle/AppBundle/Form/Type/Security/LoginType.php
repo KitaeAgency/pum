119b5757585bc1645fe224783f0283fe5fcd02fd
Phuky <tpt@les-argonautes.com>
2015-01-21 15:03:21 +0100

    translations login form

--- src/Pum/Bundle/AppBundle/Form/Type/Security/LoginType.php
+++ src/Pum/Bundle/AppBundle/Form/Type/Security/LoginType.php
@@ -4,17 +4,25 @@ namespace Pum\Bundle\AppBundle\Form\Type\Security;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class LoginType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('_username', 'text', array('attr' => array('placeholder' => 'Username')))
-            ->add('_password', 'password', array('attr' => array('placeholder' => 'Password')))
-            ->add('_remember_me', 'checkbox', array('required' => false))
-            ->add('submit', 'submit', array('label' => 'Signin'))
+            ->add('_username', 'text', array('attr' => array('placeholder' => 'pum.login.email')))
+            ->add('_password', 'password', array('attr' => array('placeholder' => 'pum.login.pwd')))
+            ->add('_remember_me', 'checkbox', array('label' => 'pum.login.rememberme', 'required' => false))
+            ->add('submit', 'submit', array('label' => 'pum.login.login'))
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'translation_domain' => 'pum_form',
+        ));
     }
 
     public function getName()
