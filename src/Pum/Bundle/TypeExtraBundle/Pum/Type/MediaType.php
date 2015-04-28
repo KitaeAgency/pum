bd980d887d1be14a15c8e3afcb6bbc1a84b0c924
Yoann Morocutti <ym@les-argonautes.com>
2015-04-28 18:27:38 +0200

    Fix storageToRemove media property

--- src/Pum/Bundle/TypeExtraBundle/Pum/Type/MediaType.php
+++ src/Pum/Bundle/TypeExtraBundle/Pum/Type/MediaType.php
@@ -32,7 +32,7 @@ class MediaType extends AbstractType
         $cb->createProperty($camel.'_file'); // not persisted
 
         if (!$cb->hasProperty('storageToRemove')) {
-            $cb->createProperty('storageToRemove', 'null');
+            $cb->createProperty('storageToRemove', null);
         }
         if (!$cb->hasMethod('getStorageToRemove')) {
             $cb->createMethod('getStorageToRemove', '', 'return $this->storageToRemove;');
@@ -111,7 +111,6 @@ class MediaType extends AbstractType
         $removeFromStorageCode = '$storage->remove($this->'.$camel.'_id);';
 
         if (!$cb->hasImplements('Pum\Bundle\TypeExtraBundle\Media\FlushStorageInterface')) {
-
             $cb->addImplements('Pum\Bundle\TypeExtraBundle\Media\FlushStorageInterface');
             $cb->createMethod('flushToStorage', 'Pum\Bundle\TypeExtraBundle\Media\StorageInterface $storage', $flushToStorageCode);
             $cb->createMethod('removeFromStorage', 'Pum\Bundle\TypeExtraBundle\Media\StorageInterface $storage, $removeAll = false', 'if (false === $removeAll) {
